49a228471bd62174f23f9dcedd638c0bf7f616ce
rahulpargi641 <rahulpargi641@gmail.com>
2022-04-11 17:20:06 +0530

    Exploring Neighbour

--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -7,11 +7,40 @@ public class PathFinder : MonoBehaviour
 {
     [SerializeField] WayPoint startWayPoint, EndWayPoint;
     Dictionary<Vector2, WayPoint> grid = new Dictionary<Vector2, WayPoint>();
+
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
         ColorStartAndEnd();
+        ExploreNeighbours();
+    }
+
+    private void ExploreNeighbours()
+    {
+        foreach(Vector2Int direction in directions)
+        {
+            Vector2Int explorationCordinates = startWayPoint.GetGridPos() + direction;
+            print("print" + explorationCordinates);
+            try
+            {
+                grid[explorationCordinates].SetTopColor(Color.blue);
+
+            }
+            catch
+            {
+
+            }    
+        }
+            
+
     }
 
     private void ColorStartAndEnd()
